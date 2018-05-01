49e84a9ac275593e3be7dfb9e2799055d5b422f1
paulomilitante <paulo.militante@gmail.com>
2018-05-01 16:21:59 +0800

    Final Draft

--- rsvp-email.php
+++ rsvp-email.php
@@ -28,9 +28,9 @@ $apiKey = getenv('SENDGRID_API_KEY');
 $sg = new \SendGrid($apiKey);
 
 $response = $sg->client->mail()->send()->post($mail);
-echo $response->statusCode();
-echo $response->headers();
-echo $response->body();
+// echo $response->statusCode();
+// echo $response->headers();
+// echo $response->body();
 
 include("success.html");
 
