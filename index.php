1af3ecfca3915bbcedb180fb741c6726928a2064
paulomilitante <paulo.militante@gmail.com>
2018-05-01 15:33:36 +0800

    added password protect JS

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-<?php header( 'Location: /index.html' ) ;  ?>
+<?php
+$pass = $_POST['pass'];
+
+if($pass == "10062018")
+{
+        include("wedding.html");
+}
+else
+{
+    if(isset($_POST))
+    {?>
+
+            <form method="POST">
+            Pass <input type="password" name="pass"></input><br/>
+            <input type="submit" name="submit" value="Go"></input>
+            </form>
+    <?}
+}
+?>
